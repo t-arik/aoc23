1a8f2b147c70de9e6baec2dcc10f3526ab0b17bd
Tarik <tarik.badawy@studium.uni-hamburg.de>
2023-12-15 22:19:39 +0100

    day 12 correction

--- 12.exs
+++ 12.exs
@@ -11,6 +11,7 @@ defmodule Day12 do
       {_, [0 | groups]} -> arrangements({springs, groups})
       {"", _} -> 0
       {"#" <> "." <> rest_s, [1 | rest_g]} -> arrangements({rest_s, rest_g})
+      {"#" <> "." <> _, _} -> 0
       {"#" <> "?" <> rest_s, [1 | rest_g]} -> arrangements({"." <> rest_s, rest_g})
       {"#" <> "?" <> rest_s, [group | rest_g]} -> arrangements({"#" <> rest_s, [group - 1 | rest_g]})
       {"#" <> rest_s, [size | rest_g]} -> arrangements({rest_s, [size - 1 | rest_g]})
